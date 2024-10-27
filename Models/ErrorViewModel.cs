e1b98a4a236f026cdfa156ee1e4376a97eee9b38
AdityaWebsmith <adityajoshi@websmithsolution.com>
2024-10-27 20:46:54 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinuxDeploymentTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
